485a6676f67d27596bfd1bdf412f038a270a475e
jadhavaarya14 <jadhavaarya1417@gmail.com>
2026-02-24 18:31:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -128,3 +128,41 @@ cards.forEach(card => {
         card.style.transform = "rotateX(0) rotateY(0)";
     });
 });
+
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("mousemove", e => {
+        const rect = btn.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
+
+        btn.style.transform = `translate(${x * 0.2}px, ${y * 0.2}px)`;
+    });
+
+    btn.addEventListener("mouseleave", () => {
+        btn.style.transform = `translate(0px, 0px)`;
+    });
+});
+const heroSpans = document.querySelectorAll(".hero h1 span");
+
+heroSpans.forEach((span, i) => {
+    span.style.transitionDelay = `${i * 0.1}s`;
+    span.classList.add("active");
+});
+if(toggle){
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
+
+const courseCards = document.querySelectorAll('.course-card');
+
+window.addEventListener('scroll', () => {
+    courseCards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        if(cardTop < window.innerHeight - 100){
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
+
